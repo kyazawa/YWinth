3846f303cca4130862ab8a9b39854c70d9c388ca
Yazawa <s14138@numazu.kosen-ac.jp>
2018-10-25 10:48:26 +0900

    運指テーブル参照バグ修正 PROGMEMテーブル参照時は，関数 pgm_read_wordを呼んで読み出す 配列として読むとバグる

--- Finger.cpp
+++ Finger.cpp
@@ -9,6 +9,9 @@
 
 uint16_t fingerToNoteNum(uint8_t finger){
 	uint16_t noteNum;
-	noteNum = FINGER_TBL[(int)finger];
+	//noteNum = FINGER_TBL[fingerI];
+	noteNum = pgm_read_word(&FINGER_TBL[finger]);
+	/* PROGMEMﾃｰﾌﾞﾙ参照時は，?の関数を読んで読み出す！！
+	   配列として読むと変な値が出るので注意する！ */
 	return noteNum;
 }
